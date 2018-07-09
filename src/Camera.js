bd1b85849c36cb1424aa2cc9fb32aee7acf2ec11
Timmo <28114703+timmo001@users.noreply.github.com>
2018-07-09 18:04:15 +0100

    :hammer: remove appbar and click to dismiss

--- src/Camera.js
+++ src/Camera.js
@@ -2,11 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
 
 const styles = theme => ({
   appBar: {
@@ -25,7 +20,12 @@ const styles = theme => ({
     position: 'absolute',
     height: '100%',
     width: '100%',
-  }
+  },
+  cameraOverlay: {
+    position: 'fixed',
+    height: '100%',
+    width: '100%',
+  },
 });
 
 class Camera extends React.Component {
@@ -34,25 +34,11 @@ class Camera extends React.Component {
     const { classes, handleClose } = this.props;
     const { data } = this.props.data;
     return (
-      <Dialog
-        open
-        fullScreen
-        onClose={handleClose}>
-        <AppBar className={classes.appBar}>
-          <Toolbar>
-            <IconButton color="inherit"
-              onClick={handleClose}
-              aria-label="Close">
-              <CloseIcon />
-            </IconButton>
-            <Typography variant="title" color="inherit" className={classes.flex}>
-              {data.name}
-            </Typography>
-          </Toolbar>
-        </AppBar>
+      <Dialog open fullScreen>
         <div className={classes.camera}>
           <iframe className={classes.cameraContent} title={data.title} src={data.url} scrolling="no" frameBorder="0" />
         </div>
+        <div className={classes.cameraOverlay} onClick={handleClose} />
       </Dialog>
     );
   }
