8b71e3f6ebab147bbd062fa7788f3d4bd8cf97a8
Timmo <28114703+timmo001@users.noreply.github.com>
2018-07-18 15:52:37 +0100

    :sparkles: UI Fixes (#13)
    * :hammer: fix more info to only show on lights that are on
    
    * :hammer: fix crash on missing component and make cards square
    
    * :hammer: fix overflow on group name
    
    * :arrow_up: update packages
    
    * :hammer: update light theme on color
    
    * :hammer: fix lint error
    
    * :hammer: make camera card also square
    
    * :hammer: switch camera image to use minutes instead of updating every second
    
    * :sparkles: change cards to use button base to show ripple

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import classnames from 'classnames';
+import moment from 'moment';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
 import config from 'config.json';
 import Camera from './Components/Camera';
 import Header from './Components/Header';
@@ -52,19 +54,30 @@ const styles = theme => ({
     width: '50%',
     padding: theme.spacing.unit / 2,
   },
-  card: {
-    cursor: 'pointer',
+  cardOuter: {
     minHeight: '8rem',
     height: '100%',
+    width: '100%',
+    textAlign: 'start',
+  },
+  card: {
+    minHeight: '8rem',
+    height: '100%',
+    width: '100%',
   },
   cardOn: {
     backgroundColor: theme.palette.backgrounds.dark,
+  },
+  cardUnavailable: {
+    backgroundColor: theme.palette.backgrounds.disabled,
   },
   cardContent: {
     height: '100%',
     padding: theme.spacing.unit * 2,
   },
   name: {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
     fontSize: '1.2rem',
   },
   state: {
@@ -78,6 +91,7 @@ const styles = theme => ({
     padding: theme.spacing.unit / 2,
   },
   camera: {
+    display: 'block',
     width: '100%',
     height: '100%',
   }
@@ -131,8 +145,11 @@ class Main extends React.Component {
     this.props.setTheme(value);
   });
 
-  handleButtonPress = (entity) => this.buttonPressTimer =
-    setTimeout(() => this.setState({ moreInfo: entity }), 1000);
+  handleButtonPress = (domain, entity) => {
+    if (domain === 'light' && entity.state === 'on')
+      this.buttonPressTimer =
+        setTimeout(() => this.setState({ moreInfo: entity }), 1000);
+  };
 
   handleButtonRelease = () => clearTimeout(this.buttonPressTimer);
 
@@ -174,49 +191,60 @@ class Main extends React.Component {
                       {group.cards.map((card, y) => {
                         const type = !card.type ? 'hass' : card.type;
                         if (type === 'hass') {
-                          const entity = entities.find(i => { return i[1].entity_id === card.entity_id })[1];
-                          const { entity_id, state, attributes } = entity;
-                          const domain = entity_id.substring(0, entity_id.indexOf('.'));
-                          return (
-                            <Grid key={y} className={classes.cardContainer} item>
-                              <Card className={classnames(
-                                classes.card,
-                                state === 'on' ? classes.cardOn : classes.cardOff
-                              )} elevation={1} onClick={() => {
-                                if (domain === 'light' || domain === 'switch')
-                                  handleChange(domain, state === 'on' ? false : true, { entity_id });
-                                else if (domain === 'scene' || domain === 'script')
-                                  handleChange(domain, true, { entity_id });
-                              }}
-                                onTouchStart={() => this.handleButtonPress(entity)}
-                                onMouseDown={() => this.handleButtonPress(entity)}
-                                onTouchEnd={this.handleButtonRelease}
-                                onMouseUp={this.handleButtonRelease}>
-                                <CardContent className={classes.cardContent}>
-                                  <Typography className={classes.name} variant="headline">
-                                    {card.name ? card.name : attributes.friendly_name}
-                                  </Typography>
-                                  {domain === 'sensor' &&
-                                    <Typography className={classes.state} variant="body1">
-                                      {state}
-                                    </Typography>
-                                  }
-                                </CardContent>
-                              </Card>
-                            </Grid>
-                          );
+                          const entity_outer = entities.find(i => { return i[1].entity_id === card.entity_id });
+                          if (entity_outer) {
+                            const entity = entity_outer[1];
+                            const { entity_id, state, attributes } = entity;
+                            const domain = entity_id.substring(0, entity_id.indexOf('.'));
+                            return (
+                              <Grid key={y} className={classes.cardContainer} item>
+                                <ButtonBase
+                                  className={classes.cardOuter}
+                                  focusRipple
+                                  disabled={state === 'unavailable'}
+                                  onClick={() => {
+                                    if (domain === 'light' || domain === 'switch')
+                                      handleChange(domain, state === 'on' ? false : true, { entity_id });
+                                    else if (domain === 'scene' || domain === 'script')
+                                      handleChange(domain, true, { entity_id });
+                                  }}
+                                  onTouchStart={() => this.handleButtonPress(domain, entity)}
+                                  onMouseDown={() => this.handleButtonPress(domain, entity)}
+                                  onTouchEnd={this.handleButtonRelease}
+                                  onMouseUp={this.handleButtonRelease}>
+                                  <Card className={classnames(
+                                    classes.card,
+                                    state === 'on' ? classes.cardOn : state === 'unavailable' ? classes.cardUnavailable : classes.cardOff
+                                  )} elevation={1} square>
+                                    <CardContent className={classes.cardContent}>
+                                      <Typography className={classes.name} variant="headline">
+                                        {card.name ? card.name : attributes.friendly_name}
+                                      </Typography>
+                                      {domain === 'sensor' &&
+                                        <Typography className={classes.state} variant="body1">
+                                          {state}
+                                        </Typography>
+                                      }
+                                    </CardContent>
+                                  </Card>
+                                </ButtonBase>
+                              </Grid>
+                            );
+                          } else return null;
                         } else if (type === 'camera') {
                           const { name, url } = card;
-                          const still_url = `${card.still_url}?${new Date().getTime()}`;
+                          const still_url = `${card.still_url}?${moment().format('HHmm')}`;
                           return (
                             <Grid key={y} className={classes.cameraContainer} item>
-                              <Card className={classes.card} elevation={1}>
-                                <img
-                                  className={classes.camera}
-                                  src={still_url}
-                                  alt={name}
-                                  onClick={() => this.handleShowCamera(name, still_url, url)} />
-                              </Card>
+                              <ButtonBase className={classes.cardOuter} focusRipple
+                                onClick={() => this.handleShowCamera(name, still_url, url)}>
+                                <Card className={classes.card} elevation={1} square>
+                                  <img
+                                    className={classes.camera}
+                                    src={still_url}
+                                    alt={name} />
+                                </Card>
+                              </ButtonBase>
                             </Grid>
                           );
                         } else { return null; }
