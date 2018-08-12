71558e5c6b485313e93f6a0f51dcf669496ba71b
Timmo <28114703+timmo001@users.noreply.github.com>
2018-08-12 23:42:46 +0100

    :hammer: fix ci test

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,15 @@
 const localStorageMock = {
   getItem: jest.fn(),
   setItem: jest.fn(),
+  removeItem: jest.fn(),
   clear: jest.fn()
 };
+const sessionStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn()
+};
+
 global.localStorage = localStorageMock;
+global.sessionStorage = sessionStorageMock;
