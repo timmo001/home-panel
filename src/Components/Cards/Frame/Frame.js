2fce3f79e7eb1d8383b880b46e43bca5737a15e3
Timmo <contact@timmo.xyz>
2018-09-11 20:32:41 +0100

    :sparkles: add height customisation support to iframe

--- src/Components/Cards/Frame/Frame.js
+++ src/Components/Cards/Frame/Frame.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import ButtonBase from '@material-ui/core/ButtonBase';
 import Card from '@material-ui/core/Card';
 import { getCardElevation, getSquareCards } from '../../Common/config';
 import card from '../../Common/Style/card';
 
 const styles = theme => ({
   ...card(theme),
+  frameContainer: {
+    minHeight: 'var(--height)',
+    height: 'var(--height)',
+  },
+  frameInnerContainer: {
+    height: '100%'
+  },
   frame: {
     display: 'block',
     width: '100%',
-    height: 'calc(100% - 64px)',
+    height: '100%',
     border: 0,
   },
 });
@@ -25,25 +32,28 @@ class Frame extends React.Component {
     const squareCards = getSquareCards(config);
     return (
       <Grid
-        className={classes.cardContainer}
+        className={classnames(classes.cardContainer, classes.frameContainer)}
         style={{
           '--width': card.width ?
             typeof card.width === 'number'
               ? `calc(130px * ${card.width})`
               : card.width
+            : '260px',
+          '--height': card.height ?
+            typeof card.height === 'number'
+              ? `calc(130px * ${card.height})`
+              : card.height
             : '260px'
         }}
         item>
-        <ButtonBase className={classes.cardOuter} focusRipple>
-          <Card className={classes.card} elevation={cardElevation} square={squareCards}>
-            <iframe
-              className={classes.frame}
-              title={name}
-              src={url}
-              sandbox="allow-forms allow-popups allow-pointer-lock allow-same-origin allow-scripts allow-presentation"
-              allowFullScreen={true} />
-          </Card>
-        </ButtonBase>
+        <Card className={classes.frameInnerContainer} elevation={cardElevation} square={squareCards}>
+          <iframe
+            className={classes.frame}
+            title={name}
+            src={url}
+            sandbox="allow-forms allow-popups allow-pointer-lock allow-same-origin allow-scripts allow-presentation"
+            allowFullScreen={true} />
+        </Card>
       </Grid>
     );
   }
