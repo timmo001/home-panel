78ddfb7f5c50450b3111737c6109ac23e4c861e6
Timmo <contact@timmo.xyz>
2018-09-13 20:27:52 +0100

    :hammer: add test

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,13 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('logs in without crashing', () => {
+  localStorage.setItem('username', process.env.REACT_APP_OVERRIDE_USERNAME);
+  localStorage.setItem('password', process.env.REACT_APP_OVERRIDE_PASSWORD);
+  localStorage.setItem('api_url', process.env.REACT_APP_OVERRIDE_API_URL);
+  localStorage.setItem('hass_url', process.env.REACT_APP_OVERRIDE_HASS_URL);
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
