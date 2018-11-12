0c0ad58ae528a313532357dc0e1a55771359c8c8
Timmo <contact@timmo.xyz>
2018-11-12 17:16:12 +0000

    :sparkles: add main weather content

--- src/Components/Cards/Hass/Weather.js
+++ src/Components/Cards/Hass/Weather.js
@@ -1,5 +1,6 @@
 import React from 'react';
-// import classnames from 'classnames';
+import classnames from 'classnames';
+import moment from 'moment';
 import PropTypes from 'prop-types';
 import ReactAnimatedWeather from 'react-animated-weather-updated';
 import withStyles from '@material-ui/core/styles/withStyles';
@@ -8,10 +9,53 @@ import Grid from '@material-ui/core/Grid';
 import properCase from '../../Common/properCase';
 
 const styles = theme => ({
+  main: {
+    display: 'inline-flex',
+    alignItems: 'center'
+  },
+  temperature: {
+    display: 'flex'
+  },
   unit: {
     marginLeft: theme.spacing.unit / 2
+  },
+  icon: {
+    color: theme.palette.text.icon,
+    fontSize: 64
+  },
+  forecast: {
+  },
+  forecastItem: {
+    width: 60
   }
 });
+
+const weatherMap = {
+  "clear-night": "weather-night",
+  cloudy: "weather-cloudy",
+  fog: "weather-fog",
+  hail: "weather-hail",
+  lightning: "weather-lightning",
+  "lightning-rainy": "weather-lightning-rainy",
+  partlycloudy: "weather-partlycloudy",
+  pouring: "weather-pouring",
+  rainy: "weather-rainy",
+  snowy: "weather-snowy",
+  "snowy-rainy": "weather-snowy-rainy",
+  sunny: "weather-sunny",
+  windy: "weather-windy",
+  "windy-variant": "weather-windy-variant",
+};
+
+const params = {
+  slidesPerView: '5',
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+};
 
 class Weather extends React.Component {
 
@@ -32,39 +76,49 @@ class Weather extends React.Component {
   }
 
   render() {
-    const { classes, theme, state, attributes } = this.props;
-    // console.log(state);
-    // console.log(attributes);
+    const { classes, state, attributes } = this.props;
+    const icon = weatherMap[state];
+
     return (
       <Grid
         container
-        direction="column">
-        <Grid item>
-          {/* <ReactAnimatedWeather
-            icon={state}
-            color={theme.palette.text.main}
-            size={80}
-            animate={true} /> */}
-
-          <Typography variant="h4">
+        direction="column"
+        justify="space-between">
+        <Grid
+          item
+          container
+          spacing={8}
+          direction="row"
+          justify="space-between">
+          <Grid item xs className={classes.main}>
+            <i className={classnames('mdi', `mdi-${icon}`, classes.icon)} />
+            <Grid item xs>
+              <Typography variant="subtitle1">
+                {state}
+              </Typography>
+              <div className={classes.temperature}>
+                <Typography variant="h5">
             {attributes.temperature}
           </Typography>
-          <Typography className={classes.unit} variant="h5">
+                <Typography className={classes.unit} variant="subtitle1">
             {this.getUnit('temperature')}
           </Typography>
-
+              </div>
+            </Grid>
         </Grid>
-        <Grid
-          container
-          direction="column"
-          justify="space-between">
-          {Object.keys(attributes).filter(i => typeof attributes[i] == 'number')
-            .map((attribute, i) => {
+          <Grid item xs>
+            <div className={classes.attributes}>
+              {delete attributes.temperature && Object.keys(attributes).filter(i =>
+                typeof attributes[i] == 'number'
+              ).map((attribute, i) => {
               return (
-                <Grid key={i} item>
-                  <Typography variant="subtitle1">
+                  <Typography key={i} className={classes.attribute} variant="body1">
                     {properCase(attribute)}: {attributes[attribute]} {this.getUnit(attribute)}
                   </Typography>
+                );
+              })}
+            </div>
+          </Grid>
                 </Grid>
               );
             })}
