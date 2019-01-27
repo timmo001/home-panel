a4dd2ee682e7145c2ba1d4a7b8b0625194c435d4
Timmo <contact@timmo.xyz>
2019-01-27 10:51:07 +0000

    :boat: :books: Update docker setup

--- docker/rootfs/etc/cont-init.d/10-nginx.sh
+++ docker/rootfs/etc/cont-init.d/10-nginx.sh
@@ -3,7 +3,7 @@
 # Configures NGINX for use with Home Panel
 # ==============================================================================
 # Enable SSL
-if [ -f "$SSL_CERTFILE" ]; then
+if [[ -z "$SSL_CERTFILE" ]]; then
     sed -i "s/%%certfile%%/${SSL_CERTFILE}/g" /etc/nginx/nginx-ssl.conf
     sed -i "s/%%keyfile%%/${SSL_KEYFILE}/g" /etc/nginx/nginx-ssl.conf
 fi
