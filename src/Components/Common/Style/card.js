694a730d8e3c9f6cd9c486cc295334b3f6caf30f
Timmo <contact@timmo.xyz>
2019-02-15 19:46:22 +0000

    :sparkles: Add card overlay jss

--- src/Components/Common/Style/card.js
+++ src/Components/Common/Style/card.js
@@ -54,6 +54,13 @@ const styles = (theme) => ({
     position: 'absolute',
     top: 0,
     right: 0
+  },
+  editOverlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%'
   }
 });
 
