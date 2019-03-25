45df8261bcf780d4fe7fb68b74c37a5a3c4cec76
Timmo <contact@timmo.xyz>
2019-03-25 13:51:22 +0000

    :hammer: Cleanup

--- src/Components/Common/cleanupObject.js
+++ src/Components/Common/cleanupObject.js
@@ -1,21 +1,13 @@
-import clone from './clone';
-
 const cleanupObject = obj => {
-  console.log('obj in:', clone(obj));
   Object.keys(obj).forEach(key => {
-    console.log('obj[key] in:', clone(obj[key]));
     if (obj[key] && Array.isArray(obj[key]))
       obj[key].map((i, x) => {
-        console.log('i in:', clone(i));
         i === null ? obj[key].splice(x, 1) : cleanupObject(i);
-        console.log('i out:', clone(i));
         return i;
       });
     else if (obj[key] && typeof obj[key] === 'object') cleanupObject(obj[key]);
     else obj[key] === null && delete obj[key];
-    console.log('obj[key] out:', clone(obj[key]));
   });
-  console.log('obj out:', clone(obj));
   return obj;
 };
 
