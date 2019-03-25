e88b6382198b54c10fbf9f57ae25614377ab148a
Timmo <contact@timmo.xyz>
2019-03-25 16:58:33 +0000

    :hammer: Remove logs

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -179,16 +179,12 @@ class Main extends React.PureComponent {
   };
 
   handlePageEditDone = (path, page) => {
-    console.log('handlePageEditDone', path, page);
     let config = clone(this.props.config);
     const pageId = clone(path).pop() + 1;
     config.pages.splice(pageId - 1, 1);
     if (path) {
       if (!page) {
         clone(config).items.map((i, x) => {
-          console.log('i, x', i, x);
-          console.log('i.page === pageId', i.page === pageId);
-          console.log('i.page > pageId', i.page > pageId);
           if (i.page === pageId) config.items.splice(x, 1);
           else if (i.page > pageId) config.items[x].page = i.page - 1;
           return config.items[x];
