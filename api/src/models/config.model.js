e71a3cccd6e96322e0795c0a563beea9dd3079a5
Timmo <contact@timmo.xyz>
2019-06-02 18:02:07 +0100

    :hammer: Update config db to v2

--- api/src/models/config.model.js
+++ api/src/models/config.model.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = function(app) {
   const dbPath = app.get('nedb');
   const Model = new NeDB({
-    filename: path.join(dbPath, 'config.db'),
+    filename: path.join(dbPath, 'config_v2.db'),
     autoload: true
   });
 
