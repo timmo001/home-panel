1472f61f5e3519b858d78cad163950f3b5790e60
Timmo <contact@timmo.xyz>
2019-06-08 11:56:05 +0100

    :hammer: Revert api

--- api/src/models/config.model.js
+++ api/src/models/config.model.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = function(app) {
   const dbPath = app.get('nedb');
   const Model = new NeDB({
-    filename: path.join(dbPath, 'config_v2.db'),
+    filename: path.join(dbPath, 'config.db'),
     autoload: true
   });
 
