6359acb51a04d376bf929e783e04bf0501cf49b4
Timmo <contact@timmo.xyz>
2019-07-07 21:15:31 +0100

    :sparkles: Add edit buttons similar to card buttons

--- src/Components/Overview/Overview.tsx
+++ src/Components/Overview/Overview.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
-import ButtonBase from '@material-ui/core/ButtonBase';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import ArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
+import ArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 import {
   ConfigProps,
@@ -17,11 +21,13 @@ import { HomeAssistantChangeProps } from '../HomeAssistant/HomeAssistant';
 import AddCard from '../Cards/AddCard';
 import AddGroup from '../Cards/AddGroup';
 import Base, { BaseProps } from '../Cards/Base';
+import ConfirmDialog from '../Utils/ConfirmDialog';
 import EditGroup from '../Configuration/EditGroup';
 import Header from './Header/Header';
 
 const useStyles = makeStyles((_theme: Theme) => ({
   title: {
+    width: '100%',
     fontWeight: 300,
     lineHeight: 1.2
   }
@@ -35,6 +41,7 @@ interface OverviewProps
 function Overview(props: OverviewProps) {
   const [currentPage, setCurrentPage] = React.useState(1);
   const [editingGroup, setEditingGroup] = React.useState();
+  const [deleteConfirm, setDeleteConfirm] = React.useState(false);
 
   const handleAddGroup = (groupKey: number) => () => {
     props.handleUpdateConfig!(['items', groupKey], defaultGroup(currentPage));
@@ -47,16 +54,20 @@ function Overview(props: OverviewProps) {
     );
   };
 
-  const handleDelete = (groupKey: number, cardKey: number) => () => {
+  const handleDelete = (groupKey: number, cardKey?: number) => () => {
     props.handleUpdateConfig!(['items', groupKey, 'cards', cardKey], undefined);
   };
 
-  const handleMoveUp = (groupKey: number, cardKey: number) => () => {
-    props.handleUpdateConfig!(['items', groupKey, 'cards', cardKey], [-1]);
+  const handleMoveUp = (groupKey: number, cardKey?: number) => () => {
+    if (cardKey)
+      props.handleUpdateConfig!(['items', groupKey, 'cards', cardKey], [-1]);
+    else props.handleUpdateConfig!(['items', groupKey], [-1]);
   };
 
-  const handleMoveDown = (groupKey: number, cardKey: number) => () => {
-    props.handleUpdateConfig!(['items', groupKey, 'cards', cardKey], [+1]);
+  const handleMoveDown = (groupKey: number, cardKey?: number) => () => {
+    if (cardKey)
+      props.handleUpdateConfig!(['items', groupKey, 'cards', cardKey], [+1]);
+    else props.handleUpdateConfig!(['items', groupKey], [+1]);
   };
 
   const handleUpdate = (groupKey: number, cardKey: number) => (data: any) => {
@@ -74,6 +85,14 @@ function Overview(props: OverviewProps) {
   const handleUpdateGroup = (groupKey: number) => (data: any) => {
     props.handleUpdateConfig!(['items', groupKey], data);
   };
+
+  function handleDeleteConfirm() {
+    setDeleteConfirm(true);
+  }
+
+  function handleConfirmClose() {
+    setDeleteConfirm(false);
+  }
 
   const groups =
     props.config.items.filter((item: any) => item.page === currentPage) || [];
@@ -107,17 +126,42 @@ function Overview(props: OverviewProps) {
             alignItems="flex-start"
             style={{ width: groupWidth * group.width + theme.spacing(6.5) }}
             spacing={1}>
-            <Grid item xs={12}>
-              <ButtonBase
-                disabled={props.editing === 1}
-                onClick={handleEditingGroup(groupKey, group)}>
+            <Grid item xs={12} container>
+              <Grid item>
                 <Typography
                   className={classes.title}
                   variant="h4"
                   component="h2">
                   {group.name}
                 </Typography>
-              </ButtonBase>
+              </Grid>
+              {props.editing === 1 && (
+                <Grid item xs container alignContent="center" justify="center">
+                  <IconButton
+                    color="primary"
+                    onClick={handleEditingGroup(groupKey, group)}>
+                    <EditIcon fontSize="small" />
+                  </IconButton>
+                  <IconButton color="primary" onClick={handleDeleteConfirm}>
+                    <DeleteIcon fontSize="small" />
+                  </IconButton>
+                  <IconButton color="primary" onClick={handleMoveUp(groupKey)}>
+                    <ArrowLeftIcon fontSize="small" />
+                  </IconButton>
+                  <IconButton
+                    color="primary"
+                    onClick={handleMoveDown(groupKey)}>
+                    <ArrowRightIcon fontSize="small" />
+                  </IconButton>
+                  {deleteConfirm && (
+                    <ConfirmDialog
+                      text="Are you sure you want to delete this card?"
+                      handleClose={handleConfirmClose}
+                      handleConfirm={handleDelete(groupKey)}
+                    />
+                  )}
+                </Grid>
+              )}
             </Grid>
             {group.cards.map((card: BaseProps, key: number) => (
               <Base
