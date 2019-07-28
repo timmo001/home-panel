4718e6a38b21e302287c9c2d704d08d564e2d38e
Timmo <contact@timmo.xyz>
2019-07-28 17:06:13 +0100

    :sparkles: One port (#368)
    * :sparkles: Working with one port
    
    * :hammer: Add suppress ssl warning
    
    For ingress or ssl rewriters
    
    * :hammer: More logging
    
    * :hammer: Relative start url

--- api/src/index.js
+++ api/src/index.js
@@ -8,18 +8,23 @@ const port = app.get('port');
 
 let server;
 if (fs.existsSync(process.env.SSL_PATH_CERT || 'fullchain.pem')) {
-  server = https.createServer(
-    {
-      cert: fs
-        .readFileSync(process.env.SSL_PATH_CERT || 'fullchain.pem')
-        .toString(),
-      key: fs.readFileSync(process.env.SSL_PATH_KEY || 'privkey.pem').toString()
-    },
-    app
-  ).listen(port);
+  server = https
+    .createServer(
+      {
+        cert: fs
+          .readFileSync(process.env.SSL_PATH_CERT || 'fullchain.pem')
+          .toString(),
+        key: fs
+          .readFileSync(process.env.SSL_PATH_KEY || 'privkey.pem')
+          .toString()
+      },
+      app
+    )
+    .listen(port);
 } else {
   server = http.createServer(app).listen(port);
-  console.warn('SSL (HTTPS) is not active!!!');
+  if (process.env.SSL_SUPPRESS_WARNING !== 'true')
+    logger.warning('SSL (HTTPS) is not active!!!');
 }
 
 app.setup(server);
@@ -28,13 +33,15 @@ process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-server.on('listening', () =>
-  logger.info(
-    'API started on %s://%s:%d',
-    fs.existsSync(process.env.SSL_PATH_CERT || 'fullchain.pem')
-      ? 'https'
-      : 'http',
-    app.get('host'),
-    port
-  )
-);
+server.on('listening', () => {
+  if (process.env.SUPPRESS_ADDRESS === 'true') logger.info('Server started');
+  else
+    logger.info(
+      'Server started on %s://%s:%d',
+      fs.existsSync(process.env.SSL_PATH_CERT || 'fullchain.pem')
+        ? 'https'
+        : 'http',
+      app.get('host'),
+      port
+    );
+});
