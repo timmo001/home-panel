02c308ae75048c9d12e11193a49a91b0e2c1bfa7
Timmo <contact@timmo.xyz>
2019-09-01 22:37:37 +0100

    :hammer: Remove path

--- src/Components/Onboarding.tsx
+++ src/Components/Onboarding.tsx
@@ -67,7 +67,7 @@ function Onboarding(props: OnboardingProps) {
       socket = io(url, { path: `${path}/socket.io`.replace('//', '/') });
       client.configure(socketio(socket));
       client.configure(authentication());
-      client.path = `${path}/socket.io`.replace('//', '/');
+      // client.path = `${path}/socket.io`.replace('//', '/');
       // TODO: Remove
       console.log('client.path:', clone(client.path));
     }
