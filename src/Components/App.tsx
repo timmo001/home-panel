3893b44709a2a12a42122b7b74ca98326a01e7b1
Timmo <contact@timmo.xyz>
2019-09-01 17:16:59 +0100

    :hammer: Test

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -20,17 +20,17 @@ console.log('APP - window.location:', clone(window.location));
 const originLocation = clone(window.location);
 
 function App() {
+  // TODO: Remove
+  console.log('APP - window.location:', clone(window.location));
+
   return (
     <Router>
       <Switch>
         <Route
-          exact
-          path="/(login|overview|configuration)/"
           render={(rProps: RouteComponentProps) => (
             <Onboarding {...rProps} originLocation={originLocation} />
           )}
         />
-        <Redirect to="overview" />
       </Switch>
     </Router>
   );
