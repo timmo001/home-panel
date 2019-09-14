1cdba1772ef3ef9c492b049674250e8920924205
Timmo <contact@timmo.xyz>
2019-09-14 22:27:24 +0100

    :ambulance: Force database to use /data

--- rootfs/etc/cont-init.d/secrets.sh
+++ rootfs/etc/cont-init.d/secrets.sh
@@ -2,4 +2,11 @@
 # ==============================================================================
 # This updates the internal auth secret for the API
 # ==============================================================================
-sed -i -e "s/API_AUTH_SECRET/$(openssl rand -base64 32)/g" /opt/panel/config/default.json
+if [ "$(grep -i 'API_AUTH_SECRET' /opt/panel/config/default.json)" = 0 ]; then
+    sed -i -e "s/API_AUTH_SECRET/$(openssl rand -base64 32)/g" /opt/panel/config/default.json
+fi
+
+if [ "$(grep -i '../db' /opt/panel/config/default.json)" = 0 ]; then
+    # Force database to use /data
+    sed -i "s#../db#/data#g" /opt/panel/config/default.json
+fi
