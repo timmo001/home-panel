1e0a88036afd4af52d6d94e34f2d489e295ecc0c
Timmo <contact@timmo.xyz>
2019-09-16 21:37:57 +0100

    :ambulance: Fix iFrame height

--- src/Components/Cards/Frame.tsx
+++ src/Components/Cards/Frame.tsx
@@ -1,18 +1,18 @@
 // @flow
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 
 import { BaseProps } from './Base';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
   root: {
     height: '100%'
   },
   frame: {
     display: 'block',
     width: '100%',
-    height: '100%',
+    height: `calc(100% - ${theme.spacing(2)}px)`,
     border: 0
   }
 }));
@@ -25,7 +25,6 @@ function Frame(props: FrameProps) {
   return (
     <iframe
       className={classes.frame}
-      style={{ height: props.card.height ? props.card.height : 'auto' }}
       title={props.card.title}
       src={props.card.url}
       sandbox="allow-forms allow-popups allow-pointer-lock allow-same-origin allow-scripts allow-presentation"
