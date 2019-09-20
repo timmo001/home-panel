bb31060196e1cd99807dc82add971ab95a914e3e
Timmo <contact@timmo.xyz>
2019-09-20 20:32:25 +0100

    :hammer: Improve performance of chart

--- src/Components/Visualisations/Chart.tsx
+++ src/Components/Visualisations/Chart.tsx
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import ApexChart from 'react-apexcharts';
@@ -33,94 +33,102 @@ interface ChartProps {
 }
 
 function Chart(props: ChartProps) {
-  const options = {
-    chart: {
-      toolbar: {
+  const [options, setOptions] = React.useState();
+  const [series, setSeries] = React.useState();
+
+  useEffect(() => {
+    setSeries(
+      props.type === 'radialBar'
+        ? [{ data: props.series[0].data[props.series[0].data.length - 1] }]
+        : props.series
+    );
+
+    setOptions({
+      chart: {
+        toolbar: {
+          show: false
+        },
+        zoom: {
+          enabled: false
+        }
+      },
+      colors: [props.color || '#607D8B'],
+      dataLabels: {
+        enabled: false
+      },
+      grid: {
+        show: false,
+        xaxis: {
+          lines: {
+            show: false
+          }
+        },
+        yaxis: {
+          lines: {
+            show: false
+          }
+        }
+      },
+      legend: {
         show: false
       },
-      zoom: {
+      plotOptions: {
+        radialBar: {
+          startAngle: -90,
+          endAngle: 90,
+          dataLabels: {
+            show: false
+          }
+        }
+      },
+      stroke: {
+        curve: 'smooth',
+        lineCap: 'butt',
+        width: 3
+      },
+      tooltip: {
         enabled: false
-      }
-    },
-    colors: [props.color || '#607D8B'],
-    dataLabels: {
-      enabled: false
-    },
-    grid: {
-      show: false,
+      },
       xaxis: {
-        lines: {
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false
+        },
+        crosshairs: {
+          show: false
+        },
+        labels: {
           show: false
         }
       },
       yaxis: {
-        lines: {
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false
+        },
+        crosshairs: {
+          show: false
+        },
+        labels: {
           show: false
         }
       }
-    },
-    legend: {
-      show: false
-    },
-    plotOptions: {
-      radialBar: {
-        startAngle: -90,
-        endAngle: 90,
-        dataLabels: {
-          show: false
-        }
-      }
-    },
-    stroke: {
-      curve: 'smooth',
-      lineCap: 'butt',
-      width: 3
-    },
-    tooltip: {
-      enabled: false
-    },
-    xaxis: {
-      axisBorder: {
-        show: false
-      },
-      axisTicks: {
-        show: false
-      },
-      crosshairs: {
-        show: false
-      },
-      labels: {
-        show: false
-      }
-    },
-    yaxis: {
-      axisBorder: {
-        show: false
-      },
-      axisTicks: {
-        show: false
-      },
-      crosshairs: {
-        show: false
-      },
-      labels: {
-        show: false
-      }
-    }
-  };
+    });
+  }, [props.color, props.series, props.type]);
 
   const classes = useStyles();
 
+  if (!options || !series) return null;
   return (
     <div className={classes.root}>
       <ApexChart
         height="100%"
         options={options}
-        series={
-          props.type === 'radialBar'
-            ? [props.series[0].data[props.series[0].data.length - 1]]
-            : props.series
-        }
+        series={series}
         type={props.type}
       />
     </div>
