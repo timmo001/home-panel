787b0e8627cd741b1160c92208726a69bf62656c
Timmo <contact@timmo.xyz>
2019-10-13 12:51:32 +0100

    :ambulance: Fix SSL on docker container
    Closes #653

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,34 @@
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
 import logger from './logger';
 import app from './app';
 
 const port = app.get('port');
-const server = app.listen(port);
+
+let server;
+const isSsl = fs.existsSync(
+  process.env.SSL_PATH_CERT || 'fullchain.pem'
+);
+if (isSsl) {
+  server = https
+    .createServer(
+      {
+        cert: fs
+          .readFileSync(process.env.SSL_PATH_CERT || 'fullchain.pem')
+          .toString(),
+        key: fs
+          .readFileSync(process.env.SSL_PATH_KEY || 'privkey.pem')
+          .toString()
+      },
+      app
+    )
+    .listen(port);
+} else {
+  server = http.createServer(app).listen(port);
+}
+
+app.setup(server);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
@@ -10,7 +36,8 @@ process.on('unhandledRejection', (reason, p) =>
 
 server.on('listening', () =>
   logger.info(
-    'Feathers application started on http://%s:%d',
+    'API started on %s://%s:%d',
+    isSsl ? 'https' : 'http',
     app.get('host'),
     port
   )
