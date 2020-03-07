2adce926f0638d9cef76d1aec887022882f9942b
Aidan Timson <contact@timmo.xyz>
2020-03-07 12:02:46 +0000

    ⬆️ Update feathersjs to v4.5.2 (#982)
    * ⬆️ Update feathersjs to v4.5.2
    
    * Add express types
    
    * Fix types
    
    * Skip lib check

--- api/src/app.ts
+++ api/src/app.ts
@@ -60,13 +60,13 @@ app.configure(channels);
 
 // Configure a middleware for 404s and the error handler
 app.use(express.notFound());
-app.use(express.errorHandler({ logger } as any));
+app.use(express.errorHandler({ logger }));
 
 app.hooks(appHooks);
 
-app.get('/*', (_req, res) =>
-  res.sendFile(path.join(app.get('public'), 'index.html'))
-);
+app.get('/*', (_req: Request, res: { sendFile: (arg0: string) => void }) => {
+  res.sendFile(path.join(app.get('public'), 'index.html'));
+});
 
 app.set('trust proxy', true);
 
