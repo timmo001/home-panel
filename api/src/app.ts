bde3743830ee6b8439ba4c44b5d1fed5b2124a67
Timmo <contact@timmo.xyz>
2020-03-07 11:21:44 +0000

    Add express types

--- api/src/app.ts
+++ api/src/app.ts
@@ -17,6 +17,7 @@ import services from './services';
 import appHooks from './app.hooks';
 import channels from './channels';
 import authentication from './authentication';
+import { Request, Response } from 'express';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const app: Application = express(feathers());
@@ -60,11 +61,11 @@ app.configure(channels);
 
 // Configure a middleware for 404s and the error handler
 app.use(express.notFound());
-app.use(express.errorHandler({ logger } as any));
+app.use(express.errorHandler({ logger }));
 
 app.hooks(appHooks);
 
-app.get('/*', (_req, res) =>
+app.get('/*', (_req: Request, res: Response) =>
   res.sendFile(path.join(app.get('public'), 'index.html'))
 );
 
