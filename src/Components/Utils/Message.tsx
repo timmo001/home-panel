ab9e8e35b6cd8d709f8a35e4b1ef7ac6f30d50d3
Timmo <contact@timmo.xyz>
2020-03-07 20:41:38 +0000

    Add padding to message

--- src/Components/Utils/Message.tsx
+++ src/Components/Utils/Message.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from 'react';
+import { useTheme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 interface MessageProps {
   type: '' | 'info' | 'warning' | 'error';
   text: string;
+  padding?: string | number;
 }
 
 function Message(props: MessageProps): ReactElement {
@@ -21,9 +23,16 @@ function Message(props: MessageProps): ReactElement {
       break;
   }
 
+  const theme = useTheme();
+
   return (
-    <Paper variant="outlined" style={{ backgroundColor: color }}>
-      <Typography component="h4" variant="h5">
+    <Paper
+      variant="outlined"
+      style={{
+        backgroundColor: color,
+        padding: props.padding ? props.padding : theme.spacing(1)
+      }}>
+      <Typography component="h4" variant="subtitle1">
         {props.text}
       </Typography>
     </Paper>
