35ceeef38c8aac58d0bb0b8cb6edf03333988641
timmo001 <timmo001@users.noreply.github.com>
2021-02-19 11:43:44 +0000

    Prettified Code!

--- frontend/src/Components/Onboarding.tsx
+++ frontend/src/Components/Onboarding.tsx
@@ -30,9 +30,10 @@ let moveTimeout: NodeJS.Timeout;
 let socket: SocketIOClient.Socket, client: feathers.Application;
 function Onboarding(): ReactElement {
   const [loginAttempt, setLoginAttempt] = useState<ProgressState>(-2);
-  const [loginCredentials, setLoginCredentials] = useState<
-    AuthenticationResult
-  >();
+  const [
+    loginCredentials,
+    setLoginCredentials,
+  ] = useState<AuthenticationResult>();
   const [config, setConfig] = useState<ConfigurationProps>();
   const [configId, setConfigId] = useState<string>();
   const [command, setCommand] = useState<CommandType>();
