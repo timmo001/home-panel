0ce5452483211161adc2f7e00154d91c7697908c
Aidan Timson <contact@timmo.xyz>
2023-05-09 18:30:58 +0100

    Add margin

--- src/components/AccessDenied.tsx
+++ src/components/AccessDenied.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 
 export function AccessDenied(): JSX.Element {
   return (
-    <Typography component="h5" variant="h2">
+    <Typography component="h5" variant="h2" sx={{ margin: "0 1rem" }}>
       Access Denied
     </Typography>
   );
