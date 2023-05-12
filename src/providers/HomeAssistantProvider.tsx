42935a5de437a3653a4e24fce46ca9882d42ca5a
Aidan Timson <contact@timmo.xyz>
2023-05-12 15:13:24 +0100

    Create provider

--- src/providers/HomeAssistantProvider.tsx
+++ src/providers/HomeAssistantProvider.tsx
@@ -1,10 +1,71 @@
 "use client";
-import type { ReactNode } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import {
+  Auth,
+  AuthData,
+  callService,
+  Connection,
+  createConnection,
+  ERR_HASS_HOST_REQUIRED,
+  ERR_INVALID_AUTH,
+  getAuth,
+  getUser,
+  HassConfig,
+  HassEntities,
+  HassUser,
+  subscribeConfig,
+  subscribeEntities,
+} from "home-assistant-js-websocket";
+
+const HomeAssistantContext = createContext<Connection | null>(null);
 
 export function HomeAssistantProvider({
   children,
 }: {
   children: ReactNode;
 }): JSX.Element {
-  return <>{children}</>;
+  const [connection, setConnection] = useState<Connection | null>(null);
+
+  useEffect(() => {
+    let connection: Connection;
+    (async () => {
+      connection = await createConnection({
+        // configuration options here
+      });
+      connection.addEventListener("ready", () => {
+        console.log("Connected to Home Assistant");
+        setConnection(connection);
+      });
+      connection.addEventListener("disconnected", () => {
+        console.log("Disconnected from Home Assistant");
+        connection.reconnect();
+        setConnection(null);
+      });
+    })();
+
+    return () => {
+      connection.close();
+    };
+  }, []);
+  return (
+    <HomeAssistantContext.Provider value={connection}>
+      {children}
+    </HomeAssistantContext.Provider>
+  );
+}
+
+export function useHomeAssistant() {
+  const connection = useContext(HomeAssistantContext);
+  if (!connection) {
+    throw new Error(
+      "useHomeAssistant must be used within a HomeAssistantProvider"
+    );
+  }
+  return connection;
 }
