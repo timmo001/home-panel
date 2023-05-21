9d24e2c569d8a03a33fd96cbc3ca7f4bc8c2fcff
Aidan Timson <contact@timmo.xyz>
2023-05-21 20:22:10 +0100

    Fix

--- src/components/dashboard/views/widgets/expanded/homeAssistant/Cover.tsx
+++ src/components/dashboard/views/widgets/expanded/homeAssistant/Cover.tsx
@@ -1,21 +1,33 @@
 "use client";
-import { useMemo } from "react";
 import { HassEntity } from "home-assistant-js-websocket";
-import { Typography } from "@mui/material";
+import { Icon } from "@mdi/react";
+import { IconButton, Typography } from "@mui/material";
+import { useMemo } from "react";
 
 import { CoverEntityFeature } from "@/utils/homeAssistant/cover";
+import { domainIcon } from "@/utils/homeAssistant/icons";
 import { entitySupportsFeature } from "@/utils/homeAssistant";
 
 export function ExpandedHomeAssistantCover({ entity }: { entity: HassEntity }) {
-  const supportsPosition = useMemo<boolean>(
-    () => entitySupportsFeature(entity, CoverEntityFeature.SET_POSITION),
+  const isOn = useMemo<boolean>(
+    () =>
+      entity.state === "open" ||
+      entity.state === "closing" ||
+      entity.state === "opening",
     [entity]
   );
 
-  const supportsTiltPosition = useMemo<boolean>(
-    () => entitySupportsFeature(entity, CoverEntityFeature.SET_TILT_POSITION),
-    [entity]
-  );
+  const isOff = useMemo<boolean>(() => entity.state === "closed", [entity]);
+
+  // const supportsPosition = useMemo<boolean>(
+  //   () => entitySupportsFeature(entity, CoverEntityFeature.SET_POSITION),
+  //   [entity]
+  // );
+
+  // const supportsTiltPosition = useMemo<boolean>(
+  //   () => entitySupportsFeature(entity, CoverEntityFeature.SET_TILT_POSITION),
+  //   [entity]
+  // );
 
   const supportsOpenClose = useMemo<boolean>(
     () =>
@@ -43,8 +55,23 @@ export function ExpandedHomeAssistantCover({ entity }: { entity: HassEntity }) {
     [entity]
   );
 
+  console.log(entity.entity_id, {
+    supportsOpenClose,
+    supportsTilt,
+    supportsOpenCloseWithoutStop,
+  });
+
   return (
     <>
+      {supportsOpenCloseWithoutStop ? (
+        <>
+          <IconButton onClick={() => {}}>
+            <Icon path={domainIcon("cover", entity, "open")} />
+          </IconButton>
+        </>
+      ) : (
+        (supportsOpenClose || supportsTilt) && <></>
+      )}
       <Typography variant="body1">
         {entity.attributes?.current_position &&
           `${entity.attributes.current_position} %`}
